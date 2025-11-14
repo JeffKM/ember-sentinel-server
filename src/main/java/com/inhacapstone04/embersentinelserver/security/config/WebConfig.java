73beee4b5cf97a8ce805b56b2849c903057abe14
Hexeong <123macanic@naver.com>
2025-11-14 18:57:14 +0900

    feat: @AuthorizedUser를 위한 Interceptor와 ArgumentResolver를 WebConfig에 등록

---
+++ src/main/java/com/inhacapstone04/embersentinelserver/security/config/WebConfig.java
@@ -0,0 +1,48 @@
+package com.inhacapstone04.embersentinelserver.security.config;
+
+import com.inhacapstone04.embersentinelserver.common.resolver.AuthorizedUserArgumentResolver;
+import com.inhacapstone04.embersentinelserver.security.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final AuthorizedUserArgumentResolver authorizedUserArgumentResolver;
+
+    /**
+     * AuthInterceptor를 스프링에 등록합니다.
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**") // (1) 모든 요청에 대해 인터셉터 실행
+                .excludePathPatterns( // (2) 단, 여기 명시된 경로는 인터셉터 실행 제외 (인증X)
+                        // --- auth(login) 관련
+                        "/auth/**",
+
+                        // --- building 관련 API
+                        "/building/**",
+
+                        // --- Swagger/API Docs ---
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-resources/**"
+                );
+    }
+
+    /**
+     * AuthorizedUserArgumentResolver를 스프링에 등록합니다.
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authorizedUserArgumentResolver);
+    }
+}
