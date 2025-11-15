016e2789aa70ca680f0094321d1d1a74c080002a
Hexeong <123macanic@naver.com>
2025-11-15 16:58:41 +0900

    feat: Room 도메인 Controller 계층 구현(page 시작을 1로 설정)

---
+++ src/main/java/com/inhacapstone04/embersentinelserver/room/controller/RoomQueryController.java
@@ -0,0 +1,42 @@
+package com.inhacapstone04.embersentinelserver.room.controller;
+
+import com.inhacapstone04.embersentinelserver.common.resolver.AuthorizedUser;
+import com.inhacapstone04.embersentinelserver.common.response.PageResponse;
+import com.inhacapstone04.embersentinelserver.room.dto.SingleRoomResponse;
+import com.inhacapstone04.embersentinelserver.room.service.RoomQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/room")
+public class RoomQueryController {
+
+    private final RoomQueryService roomQueryService;
+
+    /**
+     * [GET] /room/list/me
+     * 현재 로그인된 유저(@AuthorizedUser)와 연관된 Room의 목록을 페이징하여 조회합니다.
+     *
+     * @param userId (@AuthorizedUser 어노테이션을 통해 리졸버가 주입)
+     * @param pageable (PageableArgumentResolver가 쿼리 파라미터를 조합해 주입)
+     * @return PageResponse<SingleRoomResponse>
+     */
+    @GetMapping("/list/me")
+    public ResponseEntity<PageResponse<SingleRoomResponse>> getMyRoomsWithDefault(
+            @AuthorizedUser Long userId,
+            @PageableDefault(page = 1, size = 5) Pageable pageable
+            // page= 파라미터가 없으면 1
+            // size= 파라미터가 없으면 5
+            // page 번호는 1부터 시작
+    ) {
+
+        PageResponse<SingleRoomResponse> response = roomQueryService.getMyRooms(userId, pageable);
+        return ResponseEntity.ok(response);
+    }
+}
