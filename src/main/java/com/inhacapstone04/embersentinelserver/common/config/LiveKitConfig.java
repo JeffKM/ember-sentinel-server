2a5c43adeb9addbf12527982065251ebdb8394b7
Hexeong <123macanic@naver.com>
2025-11-23 18:37:01 +0900

    feat: Webhook 서명 검증을 위한 Receiver 빈 등록

--- src/main/java/com/inhacapstone04/embersentinelserver/common/config/LiveKitConfig.java
+++ src/main/java/com/inhacapstone04/embersentinelserver/common/config/LiveKitConfig.java
@@ -2,6 +2,7 @@ package com.inhacapstone04.embersentinelserver.common.config;
 
 import io.livekit.server.EgressServiceClient;
 import io.livekit.server.RoomServiceClient;
+import io.livekit.server.WebhookReceiver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,5 +35,15 @@ public class LiveKitConfig {
     @Bean
     public EgressServiceClient egressServiceClient() {
         return EgressServiceClient.createClient(apiUrl, apiKey, apiSecret);
+    }
+
+    /**
+     * Webhook 서명 검증을 위한 Receiver 빈 등록
+     * 이 객체는 내부적으로 API Key와 Secret을 사용하여 들어오는 요청의
+     * Authorization 헤더(JWT)를 검증합니다.
+     */
+    @Bean
+    public WebhookReceiver webhookReceiver() {
+        return new WebhookReceiver(apiKey, apiSecret);
     }
 }
