6bc08f9cfa456ef959b3f614d66e8f924498008c
Hexeong <123macanic@naver.com>
2025-12-06 21:01:36 +0900

    refactor: EnumType에 적용되지 않는 @NotBlank -> @NotNull로 수정

--- src/main/java/com/inhacapstone04/embersentinelserver/fire_event/dto/request/FireEventStartRequest.java
+++ src/main/java/com/inhacapstone04/embersentinelserver/fire_event/dto/request/FireEventStartRequest.java
@@ -2,11 +2,12 @@ package com.inhacapstone04.embersentinelserver.fire_event.dto.request;
 
 import com.inhacapstone04.embersentinelserver.fire_event.entity.DetectionType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record FireEventStartRequest(
         @NotBlank
         String deviceUuid, // 라즈베리파이의 고유 식별자 (UUID)
-        @NotBlank
+        @NotNull(message = "감지 유형(FIRE, SMOKE)은 필수입니다.")
         DetectionType detectionType
 ) {
 }
